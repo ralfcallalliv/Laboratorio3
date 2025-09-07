8ad696f080a67642f714c76b7d002c056e61439d
Ralf Callalli <ralfito1301@gmail.com>
2025-09-07 00:00:22 -0500

    Creación de clases

---
+++ src/Ejercicio1/FPSGame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class FPSGame extends Game {
+
+    public FPSGame(String name) {
+        super(name);
+    }
+
+    @Override
+    public void start() {
+        
+    }
+    
+}
